78b7332bfe6af1bdaf53d8af85cb64543c7886e2
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:30:58 +0900

    511

---
+++ Jungol/src/q511/Main.java
@@ -0,0 +1,30 @@
+package q511;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//두 개의 정수형 변수를 선언하고 값을 대입하여 아래와 같이 출력되는 프로그램을 작성하라.
+		
+		/*
+		 * Scanner sc = new Scanner(System.in);
+		 * 
+		 * int a = sc.nextInt();
+		 * int b = sc.nextInt();
+		 */
+		
+			int a;
+			int b;
+			
+			a = 55;
+			b = 10;
+			
+			System.out.println(a + "-" + b + "=" + (a - b));
+		
+			a = 2008;
+			b = 1999;
+			
+			System.out.println(a + "-" + b + "=" + (a - b));
+	}
+
+}
