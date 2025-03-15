fcd70f7b7a61004766c7cd755e73a561b37ee735
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 16:59:37 +0900

    first commit

---
+++ FirstProj/src/First.java
@@ -0,0 +1,21 @@
+public class First {
+
+	public static void main(String[] args) {
+		
+	//정수형 변수 2개를 선언하여 -1과 100을 대입한 후 아래와 같이 출력하는 프로그램을 작성하라.	
+		int a;
+		int b;
+		
+		a = -1;
+		b = 100;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		
+	}
+	
+	
+	
+}
